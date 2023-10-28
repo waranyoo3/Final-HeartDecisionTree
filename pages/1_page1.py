af818c8ca20d56b347751c440f1fe4fb2a9ed37a
waranyoo3 <633230013@Webmail.npru.ac.th>
2023-10-28 13:36:14 +0700

    d

--- pages/1_page1.py
+++ pages/1_page1.py
@@ -2,14 +2,6 @@ import json
 import time
 import requests
 import streamlit as st
-import tkinter as tk
-
-root = tk.Tk() 
-background_image = tk.PhotoImage(file="background.png")
-background_label = tk.Label(root, image=background_image)
-background_label.place(relwidth=1, relheight=1)
-root.mainloop()
-
 
 st.set_page_config(
     page_title="Waranyoo Kunpinij Project",
